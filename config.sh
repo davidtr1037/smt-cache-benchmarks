f264823f1671314a46d2cfc1a656b1cba8568671
user <davidtr1037@gmail.com>
2019-12-08 16:28:02 +0200

    updating

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VANILLA_KLEE=
+KLEE_SMM=
+KLEE=
---
+++ make/run.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
+source ${CURRENT_DIR}/../config.sh
+
+FLAGS=""
+FLAGS+="-libc=uclibc "
+FLAGS+="-posix-runtime "
+FLAGS+="-use-forked-solver=0 "
+FLAGS+="-only-output-states-covering-new "
+FLAGS+="-search=dfs "
+FLAGS+="-allocate-determ "
+FLAGS+="-allocate-determ-start-address=0x0 "
+FLAGS+="-allocate-determ-size=4000 "
+FLAGS+="-switch-type=internal "
+#FLAGS+="-allow-external-sym-calls "
+#FLAGS+="-all-external-warnings "
+
+DEPTH=0
+K_CONTEXT=4
+
+BC_FILE=${CURRENT_DIR}/build/make.bc
+ARGS="--sym-files 1 1 -sym-stdin ${CURRENT_DIR}/make.input -r -n -R -f A"
+
+function run_klee {
+    ${VANILLA_KLEE} ${FLAGS} \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_klee_smm {
+    ${KLEE_SMM} ${FLAGS} \
+        -pts \
+        -flat-memory \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_with_rebase {
+    ${KLEE} ${FLAGS} \
+        -use-sym-addr \
+        -use-rebase \
+        -use-kcontext=${K_CONTEXT} \
+        -use-global-id=1 \
+        -use-recursive-rebase=1 \
+        -reuse-arrays=0 \
+        -reuse-segments=1 \
+        -use-context-resolve=1 \
+        -rebase-reachable=0 \
+        -reachability-depth=${DEPTH} \
+        -use-batch-rebase=0 \
+        -use-ahead-rebase=1 \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_split {
+    ${KLEE} ${FLAGS} \
+        -use-sym-addr \
+        -split-objects \
+        -split-threshold=1000 \
+        -partition-size=128 \
+        ${BC_FILE} ${ARGS}
+}
+
+ulimit -s unlimited
+
+run_klee
+run_klee_smm
+run_with_rebase
