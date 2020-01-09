896133c87cc7604d8345fa5c67f830af11b272be
user <davidtr1037@gmail.com>
2020-01-09 09:57:17 +0200

    updating m4

--- m4/run.sh
+++ m4/run.sh
@@ -6,10 +6,10 @@ source ${CURRENT_DIR}/../config.sh
 MAX_MEMORY=8000
 
 FLAGS=""
-FLAGS+="-max-memory=${MAX_MEMORY} "
 FLAGS+="-libc=uclibc "
 FLAGS+="-posix-runtime "
 FLAGS+="-search=dfs "
+FLAGS+="-max-memory=${MAX_MEMORY} "
 FLAGS+="-use-forked-solver=0 "
 FLAGS+="-only-output-states-covering-new "
 FLAGS+="-switch-type=internal "
@@ -20,6 +20,8 @@ FLAGS+="-allocate-determ-size=4000 "
 
 DEPTH=0
 K_CONTEXT=4
+SPLIT_THRESHOLD=300
+PARTITION=128
 
 BC_FILE=${CURRENT_DIR}/build/src/m4.bc
 ARGS="-sym-files 1 1 -sym-stdin ${CURRENT_DIR}/m4.input -H37 -G"
