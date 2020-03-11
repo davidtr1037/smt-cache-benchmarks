056823634f8f2a3303679627467e0b943de44b36
David Trabish <davidtr1037@gmail.com>
2020-03-11 10:27:31 +0200

    updating files

--- sqlite/run.sh
+++ sqlite/run.sh
@@ -4,11 +4,11 @@ CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/../config.sh
 source ${CURRENT_DIR}/../common.sh
 
-MAX_TIME=86400
 MAX_MEMORY=8000
 
 FLAGS=""
 FLAGS+="-libc=uclibc "
+FLAGS+="-posix-runtime "
 FLAGS+="-max-memory=${MAX_MEMORY} "
 FLAGS+="-use-forked-solver=0 "
 FLAGS+="-only-output-states-covering-new "
@@ -16,71 +16,70 @@ FLAGS+="-allocate-determ "
 FLAGS+="-allocate-determ-start-address=0x0 "
 FLAGS+="-allocate-determ-size=4000 "
 
-BC_FILE=${CURRENT_DIR}/build/test_driver.bc
-DEPTH=0
-CONTEXT_RESOLVE=1
-K_CONTEXT=4
-REUSE=1
-SPLIT_THRESHOLD=300
-PARTITION=128
 
-SIZE=15
+SIZE=10
+BC_FILE=${CURRENT_DIR}/build/test_driver.bc
+
+function run_stats {
+    search=$1
+    ${KLEE} ${FLAGS} \
+        ${search} \
+        -use-sym-addr \
+        -use-global-id=1 \
+        -collect-query-stats=1 \
+        ${BC_FILE} ${SIZE}
+}
+
+function run_klee_qc_only {
+    search=$1
+    ${KLEE} ${FLAGS} \
+        ${search} \
+        -use-sym-addr \
+        -use-cex-cache=0 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
+        ${BC_FILE} ${SIZE}
+}
 
 function run_klee {
     search=$1
-    ${VANILLA_KLEE} ${FLAGS} \
+    ${KLEE} ${FLAGS} \
         ${search} \
+        -use-sym-addr \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
         ${BC_FILE} ${SIZE}
 }
 
-function run_klee_smm {
-    search=$1
-    ${KLEE_SMM} ${FLAGS} \
-        ${search} \
-        -max-time=${MAX_TIME} \
-        -pts \
-        -flat-memory \
-        ${BC_FILE} ${SIZE}
-}
-
-function run_with_rebase {
+function run_cache_qc_only {
     search=$1
     ${KLEE} ${FLAGS} \
         ${search} \
         -use-sym-addr \
-        -use-rebase \
-        -use-kcontext=${K_CONTEXT} \
-        -use-global-id=1 \
-        -use-recursive-rebase=1 \
-        -reuse-arrays=0 \
-        -reuse-segments=${REUSE} \
-        -use-context-resolve=${CONTEXT_RESOLVE} \
-        -rebase-reachable=0 \
-        -reachability-depth=${DEPTH} \
-        -use-batch-rebase=0 \
-        -use-ahead-rebase=0 \
+        -use-cex-cache=0 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
         ${BC_FILE} ${SIZE}
 }
 
-function run_context_test {
-    for i in {0..4}; do
-        K_CONTEXT=${i}
-        run_with_rebase
-    done
-}
-
-function run_split {
+function run_cache {
+    search=$1
     ${KLEE} ${FLAGS} \
-        -search=dfs \
+        ${search} \
         -use-sym-addr \
-        -split-objects \
-        -split-threshold=${SPLIT_THRESHOLD} \
-        -partition-size=${PARTITION} \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
         ${BC_FILE} ${SIZE}
 }
 
 ulimit -s unlimited
 
+run_stats
+run_klee_qc_only
+run_cache_qc_only
 run_klee
-run_klee_smm
-run_with_rebase
+run_cache
