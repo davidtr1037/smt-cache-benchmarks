8223dd00ad3bdac5ee0a71199412e1b0dbc6ad9a
user <davidtr1037@gmail.com>
2020-03-15 22:46:12 +0200

    updating make

--- make/run.sh
+++ make/run.sh
@@ -4,7 +4,6 @@ CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/../config.sh
 source ${CURRENT_DIR}/../common.sh
 
-MAX_TIME=86400
 MAX_MEMORY=8000
 
 FLAGS=""
@@ -19,6 +18,8 @@ FLAGS+="-allocate-determ-size=4000 "
 FLAGS+="-switch-type=internal "
 #FLAGS+="-allow-external-sym-calls "
 #FLAGS+="-all-external-warnings "
+
+SEARCH="--search=dfs"
 
 BC_FILE=${CURRENT_DIR}/build/make.bc
 ARGS="--sym-files 1 1 -sym-stdin ${CURRENT_DIR}/make.input -r -n -R -f A"
