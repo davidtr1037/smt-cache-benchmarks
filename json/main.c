828ea586de8350e67488a15ab4d004dc4d58150b
David Trabish <davidtr1037@gmail.com>
2020-03-22 17:02:38 +0200

    updating

--- json/main.c
+++ json/main.c
@@ -6,9 +6,6 @@
 
 #include <json.h>
 
-//#define INPUT "{'x' : 1}"
-#define INPUT "{'x' : [1, 2], 'y' : \"A\"}"
-
 void klee_make_symbolic(void *addr, size_t nbytes, const char *name) {
 
 }
@@ -19,7 +16,6 @@ void lookup(json_object *o) {
     char *k = calloc(KSIZE + 1, 1);
     klee_make_symbolic(k, KSIZE + 1, "k");
     k[KSIZE] = '\0';
-    //json_object_object_add(o, k, json_object_new_int(123));
     json_object_object_get(o, k);
 }
 
@@ -35,27 +31,5 @@ void test() {
 
 int main(int argc, char *argv[]) {
     test();
-    return 0;
-
-    //if (argc != 2) {
-    //    return 1;
-    //}
-
-    //size_t size = strtoul(argv[1], NULL, 10);
-    //char *buf = NULL;
-    //buf = malloc(size);
-
-    //klee_make_symbolic(buf, size, "buf");
-    //buf[size - 1] = '\0';
-    ////memcpy(buf, INPUT, sizeof(INPUT));
-    //buf[0] = '{';
-    //buf[size - 2] = '}';
-
-    //json_object *object;
-    //object = json_tokener_parse(buf);
-    ////if (!object) {
-    ////    fprintf(stderr, "failed...\n");
-    ////}
-
     return 0;
 }
