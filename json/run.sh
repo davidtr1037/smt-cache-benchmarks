0292201dd981176b39c122321194c66701d7609e
user <davidtr1037@gmail.com>
2020-03-29 10:25:40 +0300

    adding file

---
+++ json/run.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
+source ${CURRENT_DIR}/../config.sh
+source ${CURRENT_DIR}/../common.sh
+
+MAX_MEMORY=8000
+
+FLAGS=""
+FLAGS+="-use-forked-solver=0 "
+FLAGS+="-libc=uclibc "
+FLAGS+="-max-memory=${MAX_MEMORY} "
+FLAGS+="-allocate-determ "
+FLAGS+="-allocate-determ-start-address=0x0 "
+FLAGS+="-only-output-states-covering-new "
+
+SEARCH="-search=dfs "
+
+BC_FILE=${CURRENT_DIR}/test_driver.bc
+N=4
+
+function run_stats {
+    ${KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-global-id=1 \
+        -collect-query-stats=1 \
+        ${BC_FILE} ${N}
+}
+
+function run_klee_qc_only {
+    ${VANILLA_KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-cex-cache=0 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
+        ${BC_FILE} ${N}
+}
+
+function run_klee {
+    ${VANILLA_KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
+        ${BC_FILE} ${N}
+}
+
+function run_cache_qc_only {
+    ${KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-cex-cache=0 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
+        ${BC_FILE} ${N}
+}
+
+function run_cache {
+    ${KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
+        ${BC_FILE} ${N}
+}
+
+ulimit -s unlimited
+
+#run_stats
+run_klee_qc_only
+run_cache_qc_only
+run_klee
+run_cache
