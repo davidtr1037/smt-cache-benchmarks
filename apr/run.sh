47271d1960d4f2d9fa784e316a68a000d0b46c55
user <davidtr1037@gmail.com>
2020-04-12 12:58:41 +0300

    updating

--- apr/run.sh
+++ apr/run.sh
@@ -27,6 +27,19 @@ function run_stats {
         ${BC_FILE} ${N}
 }
 
+function run_validation {
+    ${KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
+        -use-iso-cache=1 \
+        -collect-query-stats \
+        -validate-caching \
+        ${BC_FILE} ${N}
+}
+
 function run_klee_qc_only {
     ${VANILLA_KLEE} ${FLAGS} \
         ${SEARCH} \
