b97523e2da3167f2a42fd56090b229a8eaaff74d
David Trabish <davidtr1037@gmail.com>
2020-08-11 11:50:06 +0300

    adding expat

---
+++ expat/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <expat.h>
+
+#define INPUT "<x>a</x>"
+
+void klee_make_symbolic(void *p, size_t n, const char *name) {
+
+}
+
+void klee_assume(uintptr_t c) {
+
+}
+
+int main() {
+    int z;
+    klee_make_symbolic(&z, sizeof(z), "z");
+
+    char buffer[7];
+    klee_make_symbolic(buffer, sizeof(buffer), "buffer");
+    buffer[sizeof(buffer) - 1] = 0;
+    //buffer[0] = '<';
+    //buffer[1] = 'x';
+    //buffer[2] = '>';
+    //buffer[3] = '<';
+    //buffer[4] = '/';
+    //buffer[5] = 'x';
+    //buffer[6] = '>';
+    //buffer[4] = '<';
+    //buffer[5] = '/';
+    //buffer[6] = 'x';
+    //buffer[7] = '>';
+    //klee_assume(buffer[1] >= 'a');
+    //klee_assume(buffer[1] <= 'b');
+
+    //if (z) {
+    //    printf("z...\n");
+    //}
+
+    XML_Parser p = XML_ParserCreate(NULL);
+    XML_SetHashSalt(p, 17);
+
+    //strcpy(buffer, INPUT);
+    //if (XML_Parse(p, buffer, strlen(INPUT), XML_FALSE) == XML_STATUS_ERROR) {
+    //
+    if (XML_Parse(p, buffer, sizeof(buffer) - 1, XML_FALSE) == XML_STATUS_ERROR) {
+        //printf("error...\n");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ expat/run.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
+source ${CURRENT_DIR}/../config.sh
+source ${CURRENT_DIR}/../common.sh
+
+BC_FILE=${CURRENT_DIR}/test_driver.bc
+
+MAX_MEMORY=8000
+FLAGS=""
+FLAGS+="-max-memory=${MAX_MEMORY} "
+FLAGS+="-libc=uclibc "
+FLAGS+="-posix-runtime "
+FLAGS+="-use-forked-solver=0 "
+FLAGS+="-only-output-states-covering-new "
+FLAGS+="-simplify-sym-indices "
+FLAGS+="-allocate-determ "
+FLAGS+="-allocate-determ-start-address=0x0 "
+FLAGS+="-allocate-determ-size=4000 "
+
+SEARCH="-search=dfs"
+ARGS=
+
+function run_validation {
+    ${KLEE} ${FLAGS} ${CACHE_FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
+        -collect-query-stats \
+        -validate-caching \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_klee_qc_only {
+    ${VANILLA_KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-cex-cache=0 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_klee {
+    ${VANILLA_KLEE} ${FLAGS} \
+        ${SEARCH} \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=1 \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_cache_qc_only {
+    ${KLEE} ${FLAGS} ${CACHE_FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-cex-cache=0 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_cache {
+    ${KLEE} ${FLAGS} ${CACHE_FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-cex-cache=1 \
+        -cex-cache-try-all \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
+        ${BC_FILE} ${ARGS}
+}
+
+ulimit -s unlimited
