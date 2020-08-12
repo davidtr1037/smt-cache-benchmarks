87a36b74867a851dacbde7e4ee1d1c654e76ec35
David Trabish <davidtr1037@gmail.com>
2020-08-12 15:07:16 +0300

    fixing test driver

--- expat/main.c
+++ expat/main.c
@@ -22,12 +22,12 @@ int main(int argc, char *argv[]) {
     size_t size = strtoul(argv[1], NULL, 10);
     char *buffer = malloc(size);
     klee_make_symbolic(buffer, size, "buffer");
-    buffer[size - 1] = 0;
+    //buffer[size - 1] = 0;
 
     XML_Parser p = XML_ParserCreate("iso-8859-1");
     XML_SetHashSalt(p, 17);
 
-    if (XML_Parse(p, buffer, sizeof(buffer) - 1, XML_FALSE) == XML_STATUS_ERROR) {
+    if (XML_Parse(p, buffer, size, XML_FALSE) == XML_STATUS_ERROR) {
         return 1;
     }
 
