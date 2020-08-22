eff516cc2a437ca818913ef365ec9f75c700fd90
user <davidtr1037@gmail.com>
2020-08-22 12:24:57 +0300

    updating

--- check_overhead.py
+++ check_overhead.py
@@ -4,8 +4,6 @@ import os
 from klee_out_parser import KLEEOut
 
 
-PREFIX = "experiments-overhead"
-
 def validate(ko1, ko2):
     return ko1.instructions == ko2.instructions and ko1.paths == ko2.paths
 
@@ -18,7 +16,7 @@ def compare(ko1, ko2, program=None):
 
     print "{},{},{}".format(program, ko1.time, ko2.time)
 
-def get_dirs():
+def get_dirs(root_dir):
     dirs = []
     programs = [
         "libosip",
@@ -36,8 +34,8 @@ def get_dirs():
     ]
 
     for p in programs:
-        klee_out_dir = os.path.join(PREFIX, "klee-out-{}".format(p))
-        cache_out_dir = os.path.join(PREFIX, "cache-out-{}".format(p))
+        klee_out_dir = os.path.join(root_dir, "out-klee-{}".format(p))
+        cache_out_dir = os.path.join(root_dir, "out-cache-{}".format(p))
         if os.path.exists(klee_out_dir) and os.path.exists(cache_out_dir):
             dirs.append((p, klee_out_dir, cache_out_dir, ))
         else:
@@ -46,7 +44,11 @@ def get_dirs():
     return dirs
 
 def main():
-    for program, baseline, cache in get_dirs():
+    if len(sys.argv) != 2:
+        print "Usage: <overhead_dir>"
+        return
+
+    for program, baseline, cache in get_dirs(sys.argv[1]):
         ko1 = KLEEOut(baseline)
         ko2 = KLEEOut(cache)
         compare(ko1, ko2, program)
