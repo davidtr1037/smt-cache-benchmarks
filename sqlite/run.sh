b28065c66b98374854c033eec1fa0524968c7cca
user <davidtr1037@gmail.com>
2020-09-13 11:13:12 +0300

    updating

--- sqlite/run.sh
+++ sqlite/run.sh
@@ -68,4 +68,28 @@ function run_cache {
         ${BC_FILE} ${ARGS}
 }
 
+function run_merge {
+    ${KLEE} ${FLAGS} ${CACHE_FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-rebase=1 \
+        -use-recursive-rebase=1 \
+        -reuse-segments \
+        -use-cex-cache=0 \
+        -use-branch-cache=1 \
+        ${BC_FILE} ${ARGS}
+}
+
+function run_merge_cache {
+    ${KLEE} ${FLAGS} ${CACHE_FLAGS} \
+        ${SEARCH} \
+        -use-sym-addr \
+        -use-rebase=1 \
+        -use-recursive-rebase=1 \
+        -use-cex-cache=0 \
+        -use-branch-cache=0 \
+        -use-iso-cache=1 \
+        ${BC_FILE} ${ARGS}
+}
+
 ulimit -s unlimited
