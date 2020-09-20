268b5507154a3082def0d953210aa5507a8ed4bf
user <davidtr1037@gmail.com>
2020-09-20 09:57:49 +0300

    adding file

---
+++ run_validation_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./m4/run_validation.sh
+./make/run_validation.sh
+./sqlite/run_validation.sh
+./apr/run_validation.sh
+./libxml2/run_validation.sh
+./expat/run_validation.sh
+./bash/run_validation.sh
+./json/run_validation.sh
