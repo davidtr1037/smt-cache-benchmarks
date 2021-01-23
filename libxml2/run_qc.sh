a3e1a857a0e68e199d2ec07f9b5a79b37075ca3c
user <davidtr1037@gmail.com>
2021-01-23 20:59:57 +0200

    updating

--- libxml2/run_qc.sh
+++ libxml2/run_qc.sh
@@ -3,5 +3,8 @@
 CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/run.sh
 
-run_klee
-run_cache
+PROGRAM_DIR=${ARTIFACT_DIR}/qc_fmm/libxml2
+mkdir -p ${PROGRAM_DIR}
+
+OUTPUT_DIR=${PROGRAM_DIR}/out-klee run_klee
+OUTPUT_DIR=${PROGRAM_DIR}/out-cache run_cache
