dd64b695041ce7495c966c3312eb5d6aa8fe2be1
user <davidtr1037@gmail.com>
2021-01-23 21:04:31 +0200

    updating

--- bash/run_qc_merge.sh
+++ bash/run_qc_merge.sh
@@ -3,5 +3,8 @@
 CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/run.sh
 
-run_merge
-run_merge_cache
+PROGRAM_DIR=${ARTIFACT_DIR}/qc_dsmm/bash
+mkdir -p ${PROGRAM_DIR}
+
+OUTPUT_DIR=${PROGRAM_DIR}/out-klee run_merge
+OUTPUT_DIR=${PROGRAM_DIR}/out-cache run_merge_cache
--- bash/run_validation.sh
+++ bash/run_validation.sh
@@ -3,5 +3,8 @@
 CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/run.sh
 
-run_validation
-run_merge_validation
+PROGRAM_DIR=${ARTIFACT_DIR}/validation/bash
+mkdir -p ${PROGRAM_DIR}
+
+OUTPUT_DIR=${PROGRAM_DIR}/out-fmm run_validation
+OUTPUT_DIR=${PROGRAM_DIR}/out-dsmm run_merge_validation
